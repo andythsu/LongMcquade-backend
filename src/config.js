79abf9d8c76ca4d69c1eeb847e6562b130d59622
Andy Su <f2280c@gmail.com>
2019-03-13 12:43:43 -0400

    commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const dbConfig = {
+  host: "localhost",
+  user: "th2su",
+  password: "Winter@*%2019",
+  database: "th2su"
+};
+const serverConfig = {
   port: process.env.PORT || 3000
 };
+module.exports = { dbConfig, serverConfig };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 const config = require("./config.js");
 
-const { port } = config;
+const { dbConfig, serverConfig } = config;
 
 const { EntryApi } = require("./api");
 
-app.use("/", EntryApi);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const mysql = require("mysql");
+
+var connection = mysql.createConnection(dbConfig);
+
+console.log("connection: ", connection);
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(serverConfig.port, () =>
+  console.log(`Example app listening on port ${serverConfig.port}!`)
+);
